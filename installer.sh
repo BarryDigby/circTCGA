c7505a34c75a1c8cda7ad9c7f47290bc67db23c1
BarryDigby <58001749+BarryDigby@users.noreply.github.com>
2020-03-26 10:42:23 +0000

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Script to install envs, images
+# Barry Digby
+
+conda env create -f Conda_Environments/circseq.yml
+
+conda env create -f Conda_Environments/circexplorer2
+
+singularity pull docker://barryd237/picard
+
+singularity pull docker://barryd237/CIRIquant_v1.0.1
