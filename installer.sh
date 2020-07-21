0ab2bc44878bddcf4e62a7013c1bd887fcba3000
Barry Digby <b.digby237@gmail.com>
2020-07-21 12:27:22 +0100

    push

--- installer.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Script to install environments for analysis
-
-conda env create -f Conda_Environments/circseq.yml
-conda env create -f Conda_Environments/circexplorer2.yml
-conda env create -f Conda_Environments/ciriquant.yml
-conda env create -f Conda_Environments/KNIFE.yml
